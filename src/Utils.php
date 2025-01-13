66627daed79d5e16dab0749a60da2330c06c1dbf
Heikki Vihersalo <heikki@vihersalo.fi>
2025-01-13 10:23:16 +0200

    move paths to utils file

--- src/Utils.php
+++ src/Utils.php
@@ -65,61 +65,14 @@ final class Utils {
 	 *
 	 * @since   0.1.0
 	 * @access  public
-	 * @param   string $base_uri The base URI for the assets
-	 * @param   string $name The name of the asset
+	 * @param   string $path The path to the asset
 	 * @return  string The version of the asset
 	 */
-	public static function get_asset_version( string $base_uri, string $name ): string {
-		if ( file_exists( $base_uri . $name ) ) {
-			return filemtime( $base_uri . $name );
+	public static function get_asset_version( string $path ): string {
+		if ( file_exists( $path ) ) {
+			return filemtime( $path );
 		}
 
 		return '0.1.0';
-	}
-
-	/**
-	 * Returns the base URI for the custom post types, depending on whether it is used in a theme or a plugin.
-	 * Note that function returns the path with a trailing slash.
-	 *
-	 * @since   0.1.0
-	 * @access  public
-	 * @return  string
-	 */
-	public static function get_base_path( string $package_name ): string {
-		// If the library is used in a theme, get
-		$file_to_check = get_stylesheet_directory() . '/vendor/' . $package_name . '/dist/main.js';
-
-		if ( file_exists( $file_to_check ) ) {
-			return get_stylesheet_directory();
-		}
-
-		// If the library is used in a plugin, use plugin_dir_path( __FILE__ ) instead
-		$file_to_check = plugin_dir_path( __FILE__ ) . 'vendor/' . $package_name . '/dist/main.js';
-
-		if ( file_exists( $file_to_check ) ) {
-			return plugin_dir_path( __FILE__ ) . '/';
-		}
-	}
-
-	/**
-	 * Get the base URI for the assets
-	 *
-	 * @since   0.1.0
-	 * @return  string
-	 */
-	public static function get_base_uri( string $package_name ): string {
-		// If the library is used in a theme, get_stylesheet_directory_uri() should be used instead
-		$folder = get_stylesheet_directory_uri() . '/vendor/' . $package_name . '/dist/';
-
-		if ( file_exists( $folder . 'main.js' ) ) {
-			return $folder;
-		}
-
-		// If the library is used in a plugin, use plugin_dir_url( __FILE__ ) instead
-		$folder = plugin_dir_url( __FILE__ ) . 'vendor/' . $package_name . '/dist/';
-
-		if ( file_exists( $folder . 'main.js' ) ) {
-			return $folder;
-		}
 	}
 }
