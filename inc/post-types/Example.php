b4c38aebc96e13ead44f006cb31150e325fb8b9d
Heikki Vihersalo <heikki@vihersalo.fi>
2025-01-13 11:01:53 +0200

    clean example files

--- inc/post-types/Example.php
+++ inc/post-types/Example.php
@@ -1,24 +1,15 @@
 <?php
 /**
  * Class for registering individual custom post type
- *
- * @link       https://www.kotisivu.dev
- * @since      0.1.0
- *
- * @package    HeikkiVihersalo\CustomPostTypes\PostTypes
  */
 
-namespace HeikkiVihersalo\CustomPostTypes\PostTypes;
+namespace MyTheme\Namespace;
 
 use HeikkiVihersalo\CustomPostTypes\PostType;
 use HeikkiVihersalo\CustomPostTypes\Interfaces\PostTypeInterface;
 
 /**
  * Class for registering individual custom post type
- *
- * @since      0.1.0
- * @package    HeikkiVihersalo\CustomPostTypes\PostTypes
- * @author     Heikki Vihersalo <heikki@vihersalo.fi>
  */
 class Example extends PostType implements PostTypeInterface {
 	/**
--- inc/taxonomies/Example.php
+++ inc/taxonomies/Example.php
@@ -1,24 +1,15 @@
 <?php
 /**
  * Class for registering individual taxonomy
- *
- * @link       https://www.kotisivu.dev
- * @since      0.1.0
- *
- * @package    HeikkiVihersalo\CustomPostTypes\Taxonomies
  */
 
-namespace HeikkiVihersalo\CustomPostTypes\Taxonomies;
+namespace MyTheme\Namespace;
 
 use HeikkiVihersalo\CustomPostTypes\Taxonomy;
 use HeikkiVihersalo\CustomPostTypes\Interfaces\TaxonomyInterface;
 
 /**
  * Class for registering individual taxonomy
- *
- * @since      0.1.0
- * @package    HeikkiVihersalo\CustomPostTypes\Taxonomies
- * @author     Heikki Vihersalo <heikki@vihersalo.fi>
  */
 class Example extends Taxonomy implements TaxonomyInterface {
 	/**
