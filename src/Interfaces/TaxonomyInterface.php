56038c839a9e1200e254b28047c70b12bdf122a0
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-16 15:59:48 +0200

    update methods

--- src/Interfaces/TaxonomyInterface.php
+++ src/Interfaces/TaxonomyInterface.php
@@ -7,6 +7,8 @@
 
 namespace HeikkiVihersalo\CustomPostTypes\Interfaces;
 
+use WP_Term;
+
 /**
  * Post Type Interface
  *
@@ -22,6 +24,15 @@ interface TaxonomyInterface {
 	 * @return void
 	 */
 	public function register_custom_taxonomy(): void;
+
+	/**
+	 * Register custom taxonomy callbacks
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function register_custom_taxonomy_callbacks(): void;
 
 	/**
 	 * Custom Post Type Labels for post type
@@ -41,6 +52,15 @@ interface TaxonomyInterface {
 	 * @return bool
 	 */
 	public function public(): bool;
+
+	/**
+	 * Whether the taxonomy is publicly queryable.
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function publicly_queryable(): bool;
 
 	/**
 	 * Hierarchical
@@ -68,6 +88,75 @@ interface TaxonomyInterface {
 	 * @return bool
 	 */
 	public function show_admin_column(): bool;
+
+	/**
+	 * Show in quick edit
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function show_in_quick_edit(): bool;
+
+	/**
+	 * Meta box callback
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return string
+	 */
+	public function meta_box_cb(): string;
+
+	/**
+	 * Object types
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return string|array
+	 */
+	public function object_types(): string|array;
+
+	/**
+	 * Show image field
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function show_image_field(): bool;
+
+	/**
+	 * Get term image html
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @param WP_Term $term Term object.
+	 * @return void
+	 */
+	public function get_term_image_html( WP_Term $term ): void;
+
+	/**
+	 * Add term image
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @param int $term_id Term ID.
+	 * @param int $term_taxonomy_id Term taxonomy ID.
+	 * @return void
+	 */
+	public function add_term_image( int $term_id, int $term_taxonomy_id ): void;
+
+
+	/**
+	 * Update term image
+	 *
+	 * @since 0.2.0
+	 * @access public
+	 * @param int $term_id Term ID.
+	 * @param int $term_taxonomy_id Term taxonomy ID.
+	 * @return void
+	 */
+	public function update_term_image( int $term_id, int $term_taxonomy_id ): void;
 
 	/**
 	 * Get arguments
